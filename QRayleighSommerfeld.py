683ca279e5b6b2290829947ee22b8e9608df40b1
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2022-11-11 18:06:20 -0500

    Added RS calculations + Changed layout a little

--- QRayleighSommerfeld.py
+++ QRayleighSommerfeld.py
@@ -5,11 +5,13 @@ from PyQt5.QtCore import (pyqtSignal, pyqtSlot, pyqtProperty,
                           QObject, QThread, QDir)
 from PyQt5.QtWidgets import (QFrame, QFileDialog)
 from PyQt5.QtGui import QPixmap
+from rayleighsommerfeld import rayleighsommerfeld
 
 import os
 import numpy as np
 from PIL import Image
 from matplotlib import cm
+import cv2
 
 class QRayleighSommerfeld(QFrame):
 
@@ -19,8 +21,19 @@ class QRayleighSommerfeld(QFrame):
         dir = os.path.dirname(os.path.abspath(__file__))
         uifile = os.path.join(dir, 'QRayleighSommerfeld.ui')
         uic.loadUi(uifile, self)
-        
+
+        #handle buttons/displays
         self.openImage.clicked.connect(self.dispImage)
+        self.backPropagate.clicked.connect(self.propagate)
+
+        #handle sliders
+        self.zmax_slider.valueChanged.connect(self.updateValues)
+        self.zmin_slider.valueChanged.connect(self.updateValues)
+        self.dz_slider.valueChanged.connect(self.updateValues)
+
+        #handle checkboxes
+        self.phaseCheckbox.toggled.connect(self.handlePhaseToggled)
+        self.intensityCheckbox.toggled.connect(self.handleIntToggled)
 
 #slots
     @pyqtSlot()
@@ -31,10 +44,41 @@ class QRayleighSommerfeld(QFrame):
         pixmap = QPixmap(filename)
         image = pixmap.toImage()
         s = image.bits().asstring(1280 * 1024 * channels_count)
-        im = np.fromstring(s, dtype=np.uint8).reshape((1280, 1024, channels_count))
-        nim = (im-np.min(im))/(np.max(im)-np.min(im))
-        cim = Image.fromarray(np.uint8(cm.gist_earth(nim)*255))
-    		
+        arr = np.fromstring(s, dtype=np.uint8).reshape((1280, 1024, channels_count))
+        self.arr = arr
+
+    def updateValues(self):
+        zmax = self.zmax_slider.value()
+        zmin = self.zmin_slider.value()
+        dz = self.dz_slider.value()
+        self.zmax = zmax
+        self.zmin = zmin
+        self.dz = dz
+
+    def propagate(self):
+        zmin, zmax = self.zmin, self.zmax
+        n = int((zmax - zmin)/self.dz)
+        mpp = 0.048
+        z = np.linspace(zmin, zmax, n)/mpp
+        b = self.arr[:,:,0]
+        rs = rayleighsommerfeld(b, z, magnification = mpp, nozphase = True)
+        bz = np.abs(rs).astype(float)
+        phi = np.angle(rs - 1.)
+        self.phi = phi
+        self.bz = bz
+        if self.phase_state:
+            print(phi)
+        else:
+            print(bz)
+
+    def handlePhaseToggled(self,state):
+        phase_state = state
+        self.phase_state = state
+        
+    def handleIntToggled(self,state):
+        intensity_state = state
+        self.intensity_state = state
+        
 #properties
     @pyqtProperty(str)
     def filename(self):
